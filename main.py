b1a6f0153e3ff28cac3ba2f9ea07d7bfb1bab7b6
Nikita Konovalov <nkonovalov@mirantis.com>
2019-04-24 14:19:51 +0200

    Added free
    proper links

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ def endpoint():
     if request.form['text'] == "check":
         return check()
     if request.form['text'] == "reserve":
-        return reserve(request.form['user_name'])
+        return reserve(request.form['user_id'])
+    if request.form['text'] == "free":
+        return free(request.form['user_id'])
 
     return help()
 
@@ -37,7 +39,7 @@ def check():
     body = {"text": None}
 
     if STATUS["reserved"]:
-        body["text"] = "Citrix is reserved now by @%s. Please wait!" % STATUS["reserver"]
+        body["text"] = "Citrix is reserved now by <@%s>. Please wait!" % STATUS["reserver"]
     else:
         body["text"] = "Citrix is free. Please reserve before using!"
 
@@ -50,11 +52,29 @@ def reserve(who):
     body = {"text": None}
 
     if STATUS["reserved"]:
-        body["text"] = "Citrix is reserved now by @%s. Please wait!" % STATUS["reserver"]
+        body["text"] = "Citrix is reserved now by <@%s>. Please wait!" % STATUS["reserver"]
     else:
         STATUS["reserved"] = True
         STATUS["reserver"] = who
         body["text"] = "Citrix is yours. Please dont forget to free it when you are done!"
+
+    resp = make_response(json.dumps(body), 200)
+    resp.headers["Content-type"] = "application/json"
+
+    return resp
+
+def free(who):
+    body = {"text": None}
+
+    if !STATUS["reserved"]:
+        body["text"] = "Citrix is free. Please reserve before using!"
+
+    if who != STATUS["reserver"]:
+        body["text"] = "Citrix is reserved now by <@%s>. Ask him to free it!" % STATUS["reserver"]
+    else:
+        STATUS["reserved"] = False
+        STATUS["reserver"] = None
+        body["text"] = "Citrix is free now!"
 
     resp = make_response(json.dumps(body), 200)
     resp.headers["Content-type"] = "application/json"
