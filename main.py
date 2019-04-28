11e765e7666a9b0cea4cd6e06fa2a0182838e2bc
Nikita Konovalov <nkonovalov@mirantis.com>
2019-04-28 13:01:41 +0200

    Notify proper person

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ def notify():
             }]),
             "as_user": "true",
             "response_type": "in_channel",
-            "channel": "U03MW7287",
+            "channel": STATUS["reserver"],
             "token": BOT_TOKEN}
     url = "https://slack.com/api/chat.postMessage"
 
